8267f084afdf7d4e25f1f0aa0143d409c064af69
Andre Dias <and.ddias@gmail.com>
2020-02-25 21:48:29 -0300

    2 Formas não ideias de usar a classe de usuario do Django

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+# Forma 1 - Não é a ideal
+# Criação de classe de postagem usando usuario importado Django
+class Post(models.Model):
+    autor = models.ForeignKey(User, verbose_name='Autor', on_delete=models.CASCADE)
+    titulo = models.CharField('Título', max_length=100)
+    texto = models.TextField('Texto', max_length=400)
+
+    def __str__(self):
+        return self.titulo
+
+
+from django.conf import settings
+
+
+# Forma 2 - Ainda Não ideal
+# Criação de classe de postagem usando usuario importado Django
+class Post(models.Model):
+    autor = models.ForeignKey(settings.AUTO_USER_MODEL, verbose_name='Autor', on_delete=models.CASCADE)
+    titulo = models.CharField('Título', max_length=100)
+    texto = models.TextField('Texto', max_length=400)
+
+    def __str__(self):
+        return self.titulo
--- djangoum2/settings.py
+++ djangoum2/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = '/static/'
 STATIC_MEDIA = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'usuarios.CustomUsuario'
